7167fc80fcf9de4b3310892406439af2d1cb5c21
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-09-26 10:36:44 +0545

    widget controls in custom widgets

--- src/layout/widgets/custom/index.tsx
+++ src/layout/widgets/custom/index.tsx
@@ -1,7 +1,31 @@
+import useCurrentLayout from "@/hooks/useCurrentLayout";
 import { CustomWidgetType } from "@/types/slice/widgets";
+import { Box, IconButton } from "@mui/material";
+import { useDispatch } from "react-redux";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { currentSpaceDeleteWidget } from "@/redux/slice/layout";
 
-function CustomWidget({ url }: CustomWidgetType) {
-  return <iframe src={url} className="size-full" />;
+function CustomWidget({ url, id }: CustomWidgetType) {
+  const layout = useCurrentLayout();
+  const showControls = !layout?.locked;
+
+  return (
+    <Box className="size-full relative">
+      {showControls && <CustomWidgetControls id={id} />}
+      <iframe src={url} className="size-full" />
+    </Box>
+  );
+}
+
+function CustomWidgetControls({ id }: { id: number }) {
+  const dispatch = useDispatch();
+  return (
+    <Box className="absolute right-0 top-0 p-3">
+      <IconButton onClick={() => dispatch(currentSpaceDeleteWidget(id))}>
+        <DeleteIcon />
+      </IconButton>
+    </Box>
+  );
 }
 
 export default CustomWidget;
