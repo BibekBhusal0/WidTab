fa67eabfd3c3e19395b478aa7115d98b01c038e1
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-10-27 14:18:03 +0545

    dock component is responsive in all sides

--- src/components/footer/dock.tsx
+++ src/components/footer/dock.tsx
@@ -28,12 +28,12 @@ export const Dock = () => {
 
   return (
     <motion.div
-      onMouseMove={(e) => mousePosition.set(e.pageX)}
+      onMouseMove={(e) => mousePosition.set(e[h ? "pageX" : "pageY"])}
       onMouseLeave={() => mousePosition.set(Infinity)}
       className={cn(
-        "flex items-end gap-4",
+        "flex items-end",
         "rounded-2xl backdrop-blur-sm bg-secondaryContainer-paper",
-        h ? "flex-row mx-auto px-4 h-full" : "flex-col my-auto py-4 w-full"
+        h ? "flex-row mx-auto px-4 h-full" : "flex-col my-auto py-4 pl-0 w-full"
       )}>
       {items.map((item, index) => (
         <DockIcon mouse={mousePosition} key={index} {...item} />
@@ -43,7 +43,6 @@ export const Dock = () => {
 };
 
 type DuckIconProps = { mouse: MotionValue } & SpaceMapping;
-
 function DockIcon({ mouse, name, icon, space }: DuckIconProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { currentSpace, toolBarPosition } = useSelector(
@@ -55,36 +54,45 @@ function DockIcon({ mouse, name, icon, space }: DuckIconProps) {
   };
 
   const distanceFromMouse = useTransform(mouse, (val) => {
-    const bounds = ref.current?.getBoundingClientRect() ?? {
-      x: 0,
-      width: 0,
-      y: 0,
-      height: 0,
-    };
-    if (isCurrent()) console.log(bounds);
-
-    return h
-      ? val - bounds.x - bounds.width / 2
-      : val - bounds.y - bounds.height / 2;
+    const emptyBounds = { x: 0, width: 0, y: 0, height: 0 };
+    const bounds = ref.current?.getBoundingClientRect() ?? emptyBounds;
+    return val - bounds[h ? "x" : "y"] - bounds[h ? "width" : "height"] / 2;
   });
+
   const dispatch = useDispatch();
 
   const maxDistance = 150;
-  const minSize = 40;
-  const maxSize = 70;
+  const minScale = 1;
+  const maxScale = 1.75;
+  const minMargin = 10;
+  const maxMargin = 20;
   const springConfig = { mass: 0.1, stiffness: 150, damping: 12 };
 
-  const widthTransform = useTransform(
+  const scaleTransform = useTransform(
     distanceFromMouse,
     [-maxDistance, 0, maxDistance],
-    [minSize, maxSize, minSize]
+    [minScale, maxScale, minScale]
   );
 
-  const width = useSpring(widthTransform, springConfig);
+  const marginTransform = useTransform(
+    distanceFromMouse,
+    [-maxDistance, 0, maxDistance],
+    [minMargin, maxMargin, minMargin]
+  );
+  const margin = useSpring(marginTransform, springConfig);
+  const marginTop = h ? 0 : margin;
+  const marginLeft = h ? margin : 0;
+  const scale = useSpring(scaleTransform, springConfig);
 
   return (
-    <div
-      className="cursor-pointer"
+    <motion.div
+      style={{
+        marginLeft,
+        marginTop,
+        marginRight: marginLeft,
+        marginBottom: marginTop,
+      }}
+      className="cursor-pointer relative"
       onClick={() => {
         dispatch(changeCurrentSpace(space));
       }}>
@@ -95,17 +103,18 @@ function DockIcon({ mouse, name, icon, space }: DuckIconProps) {
         <motion.div
           ref={ref}
           style={{
-            width,
-            height: width,
+            scale,
+            transformOrigin: toolBarPosition,
+            width: "40px",
           }}
           className={cn(
-            "relative flex aspect-square items-center justify-center rounded-full",
+            "relative flex-center aspect-square rounded-full",
             "bg-white/20 shadow-lg backdrop-blur-md dark:bg-black/20 icon-full p-[20%]",
             { "text-primary-main": isCurrent() }
           )}>
           <Icon2RN icon={icon} />
         </motion.div>
       </Tooltip>
-    </div>
+    </motion.div>
   );
 }
